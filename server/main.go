bcea6afa06eb79b54fa253506e7ccab9877aa482
king <hjhj117@hotmail.com>
2022-05-14 01:08:23 +0800

    Feat: 初步编译 proto golang

--- server/main.go
+++ server/main.go
@@ -1,5 +1,49 @@
 package main
 
+import (
+	"context"
+	"log"
+	"net"
+
+	pb "server/funcs"
+
+	"google.golang.org/grpc"
+)
+
+const (
+	Address string = ":8000"
+	Network string = "tcp"
+)
+
+type FuncsServer struct {
+	pb.UnimplementedFuncsServer
+}
+
+// 实现 downloadImageToSave 方法
+func (f *FuncsServer) DownloadImageToSave(ctx context.Context, req *pb.DownloadImageRequest) (*pb.DownloadImageRespond, error) {
+	return &pb.DownloadImageRespond{Respond: &pb.Respond{
+		Code: 0, Error: "", Message: "",
+	}, DownloadImageRequest: req}, nil
+}
+
 func main() {
+
+	// 1.监听端口
+	listener, err := net.Listen(Network, Address)
+	if err != nil {
+		log.Fatalf("net.listen err: %v", err)
+	}
+	log.Println(Address, " net listening...")
+	// 2.实例化gRPC服务端
+	grpcServer := grpc.NewServer()
+
+	// 3.注册我们实现的服务 SimpleService
+	// pb(grpcServer, &SimpleService{})
+	pb.RegisterFuncsServer(grpcServer, &FuncsServer{})
+	// 4.启动gRPC服务端
+	err = grpcServer.Serve(listener)
+	if err != nil {
+		log.Fatalf("grpc server err: %v", err)
+	}
 
 }
